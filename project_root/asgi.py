27ed10da219995bd82d94d1cfa2a5fd1a171922c
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2025-03-09 23:08:03 +0530

    updated

--- project_root/asgi.py
+++ project_root/asgi.py
@@ -11,6 +11,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat_app.routing  
+from chat_app.routing import websocket_urlpatterns
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_root.settings")
 
 application = ProtocolTypeRouter(
