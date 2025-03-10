4accbbd351834892f2c56af2945a893799ec7a43
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2025-03-10 11:38:49 +0530

    updated

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@ python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput  
 
+echo "Render assigned PORT: $PORT"
+
 echo "Starting ASGI server with Daphne..."
 daphne -b 0.0.0.0 -p $PORT project_root.asgi:application 
