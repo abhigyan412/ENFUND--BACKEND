f95eb9a4e30ceed8428a923b54191532036b2377
Abhigyan Tripathi <74041526+abhigyan412@users.noreply.github.com>
2025-03-10 11:32:25 +0530

    updates

--- project_root/asgi.py
+++ project_root/asgi.py
@@ -7,18 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat_app.routing  
+
+# import chat_app.routing  
 from chat_app.routing import websocket_urlpatterns
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_root.settings")
+django.setup() 
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),  
         "websocket": AuthMiddlewareStack(
-            URLRouter(chat_app.routing.websocket_urlpatterns)  
+            URLRouter(websocket_urlpatterns)  
         ),
     }
 )
--- project_root/settings.py
+++ project_root/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",  # Django REST Framework
+    "rest_framework", 
     "auth_app",
     "drive_app",
     "chat_app",
